259da4b85c2135b26570ca08b9394a507ea57df4
AnisaFitri03 <12150120006@students.uin-suska.ac.id>
2023-05-16 19:05:01 +0700

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+
+  <h1 src="">Al-Qur'an</h1>
+  <img src="../assets/anisa.png" class="rounded mx-auto d-block" alt="...">
+
+</template>
+
+<style>
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 20px;
+  width:470px;
+  height:470px;
+}
+
+h1{
+  text-align: center;
+  font-size: 35px;
+  color: #323232;
+}
+
+</style>
